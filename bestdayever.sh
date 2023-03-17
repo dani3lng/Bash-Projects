e35a0fa1a29d2aafa434487f685e591c937e9977
Daniel Ng <dani3l.yng@gmail.com>
2023-03-16 17:16:58 -0700

    completed scripts

---
+++ bestdayever.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Good morning Daniel!!"
+
+sleep 3
+
+echo "You're looking good today Daniel!!"
+
+sleep 3
+
+echo "You have the best beard I've ever seen Daniel!!"
---
+++ himom.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Hi Mom"
+
+sleep 3 
+
+echo "Uh uh"
+
+sleep 3 
+
+echo "I can't believe that"
+
+sleep 3
+
+echo "Bye, love you too"
