ea792810e3f7ccc0f66fa24e72047951ff3b8b52
Daniel Ng <dani3l.yng@gmail.com>
2023-04-25 16:20:05 -0700

    new bash script

---
+++ bestdayever3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#assign multiple argument to variables
+name = $1
+compliment = $2
+
+echo "Good morning $name!!"
+
+sleep 3
+
+echo "You're looking good today $name!!"
+
+sleep 3
+
+echo "You have the best $compliment I've ever seen $name!!"
