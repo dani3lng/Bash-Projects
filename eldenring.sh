d301a01c5ec8a74809d17c1b5ce8a5a20542e07c
Daniel Ng <dani3l.yng@gmail.com>
2023-04-26 16:55:24 -0700

    updated

--- eldenring.sh
+++ eldenring.sh
@@ -7,7 +7,7 @@ echo "Your first beast approaches. Prepare for battle. Pick a number between 0-1
 
 read tarnished
 
-if [[ $beast == $tarnished ]];
+if [[ $beast == $tarnished ]]; then
     echo "Beast Vanquished!"
 else
     echo "You Died"
