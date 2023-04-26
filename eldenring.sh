50bce12c5c920ae04bc87eb49085eb750c4a202c
Daniel Ng <dani3l.yng@gmail.com>
2023-04-26 16:54:16 -0700

    new script

---
+++ eldenring.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#first beast battle
+beast=$(( $RANDOM % 2 ))
+
+echo "Your first beast approaches. Prepare for battle. Pick a number between 0-1. (0/1)"
+
+read tarnished
+
+if [[ $beast == $tarnished ]];
+    echo "Beast Vanquished!"
+else
+    echo "You Died"
+fi
