f0a9adb9ba3e5358102e1057eb93e4e18ee9f794
Daniel Ng <dani3l.yng@gmail.com>
2023-04-26 17:29:50 -0700

    finished

--- eldenring3.sh
+++ eldenring3.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 #two boss battles
 
+echo "Welcome tarnished. Please select your starting class:
+1 - Samurai
+2 - Prisoner
+3 - Prophet"
+
+read class
+
+case $class in 
+    1) 
+        type="Samurai"
+        hp=10
+        attack=20
+        ;;
+    2) 
+        type="Prisoner"
+        hp=20
+        attack=4
+        ;;
+    3) 
+        type="Prophet"
+        hp=30
+        attack=4
+        ;;
+esac
+
+echo "You have chosen the $type class. Your HP is $hp and your attack is $attack."
+
 #beast name
 name= "Margit"
 
