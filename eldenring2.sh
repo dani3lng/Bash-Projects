d9a2f50baa0bf265985c4e10e0219e5837ba4073
Daniel Ng <dani3l.yng@gmail.com>
2023-04-26 17:03:25 -0700

    new game done

--- eldenring2.sh
+++ eldenring2.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
+#two boss battles
 
 #beast name
 name= "Margit"
 
 #first beast battle
-beast=$(( $RANDOM % 11 ))
+beast=$(( $RANDOM % 2 ))
 
-echo "$name approaches. Prepare for battle. Pick a number between 0-10: "
+echo "Your first beast approaches. Prepare for battle. Pick a number between 0-1. (0/1)"
 
 read tarnished
 
 if [[ $beast == $tarnished ]]; then
     echo "Beast Vanquished!"
+else
+    echo "You Died"
+fi
+
+sleep 2
+
+#two beast battle
+
+echo "$name approaches. Prepare for battle. Pick a number between 0-9."
+
+read tarnished
+
+beast=$(( $RANDOM % 10 ))
+
+if [[ $beast == $tarnished ]]; then
+    echo "Beast Vanquished! Congrats fellow tarnished"
 else
     echo "You Died"
 fi
